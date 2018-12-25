1b677a280386f5ce278432c4cf1a27d3c1dc3f42
= <hannes_liik@hotmail.com>
2018-12-25 16:49:26 +0200

    Remove incorrect docstring from model, Turn off gradient computation for model

--- halfcheetah_new.py
+++ halfcheetah_new.py
@@ -3,29 +3,24 @@ import torch
 import gym
 import time
 
+import os
+#os.environ["OMP_NUM_THREADS"] = "1"
 from evolution_strategies import BasicStrategy
 from evaluators import ParallelEnvEvaluator
 from deep_neuroevolution import GAOptimizer
 from utils import Policy
 
+# Disable annoying warnings from gym
 gym.logger.set_level(40)
+
+
 # Define policy
 class HCTorchPolicy(Policy, torch.nn.Module):
     # Input and output sizes from the HalfCheetah environment
-    N_INPUTS = 17
-    N_OUTPUTS = 6
+    N_INPUTS = 17  # env.action_space.shape
+    N_OUTPUTS = 6  # env.observation_space.shape
 
     def __init__(self):
-        """
-        Original:
-        out = layers.convolution2d(out, num_outputs=32, kernel_size=8, stride=4, activation_fn=tf.nn.relu)
-        out = layers.convolution2d(out, num_outputs=64, kernel_size=4, stride=2, activation_fn=tf.nn.relu)
-        out = layers.convolution2d(out, num_outputs=64, kernel_size=3, stride=1, activation_fn=tf.nn.relu)
-        out = layers.flatten(out)
-        with tf.variable_scope("action_value"):
-            out = layers.fully_connected(out, num_outputs=512,         activation_fn=tf.nn.relu)
-            out = layers.fully_connected(out, num_outputs=num_actions, activation_fn=None)
-        """
         super().__init__()
         self.net = torch.nn.Sequential(
             torch.nn.Linear(HCTorchPolicy.N_INPUTS, 64),
@@ -45,24 +40,36 @@ class HCTorchPolicy(Policy, torch.nn.Module):
         action = self.net.forward(obs)
         return action.detach().numpy()  # Back to numpy array and return
 
+
 # Create environment factory
 def env_factory() -> gym.Env:
     return gym.make("HalfCheetah-v2")
 
+
 # Create policy factory
 def policy_factory() -> Policy:
-    return HCTorchPolicy()
+    policy = HCTorchPolicy()
+    # Tell torch that we will not calculate gradients.
+    # Up to ~10% speedup and maybe takes less memory
+    for param in policy.parameters():
+        param.requires_grad = False
+    return policy
+
 
 if __name__ == '__main__':
 
     # Create evaluator
-    evaluator = ParallelEnvEvaluator(env_factory=env_factory, times=2, n_processes=8)
+    evaluator = ParallelEnvEvaluator(env_factory=env_factory, times=2, n_processes=16)
     evolution_strategy = BasicStrategy(evaluator, policy_factory, generation_size=1000, n_elites=20, n_check_top=10,
                                        n_check_times=30, std=0.02)
+
+
     def eval_callback(results):
         rewards = [result[0] for result in results]
         print(np.mean(rewards), rewards[:10])
-    #eval_callback = lambda results: print(np.mean(results), results[:10])
+
+
+    # eval_callback = lambda results: print(np.mean(results), results[:10])
     optimizer = GAOptimizer(env_factory, policy_factory, evolution_strategy, evaluator, eval_callback=eval_callback)
     for _ in range(20):
         start = time.time()
