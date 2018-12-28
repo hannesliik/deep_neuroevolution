Fix normalization bug, Remove normalizer usages

evaluators.py
@@ -22,7 +22,6 @@ class Evaluator(ABC):
 class EnvEvaluator(Evaluator):
     def __init__(self, env_factory: Callable):
         self.env: Env = env_factory()
-        self.normalizer = ObsNormalizer(env_factory)
 
     def __call__(self, policies: List[Policy], times=1) -> Tuple[List[Tuple[float, Policy]], Policy]:
         results: List[Tuple[float, Policy]] = []
@@ -33,9 +32,8 @@ class EnvEvaluator(Evaluator):
                 done = False
                 total_reward = 0
                 while not done:
-                    obs = self.normalizer.normalize(obs)
                     action = policy(obs)
-                    obs, reward, done, _ = self.env.step(action)
+                    obs, reward, done = self.env.step(action)[:3]
                     total_reward += reward
                 rewards.append(total_reward)
             results.append((int(np.mean(rewards)), policy))
@@ -46,7 +44,6 @@ class EnvEvaluator(Evaluator):
 class ParallelEnvEvaluator(Evaluator):
     def __init__(self, env_factory: Callable, times=1, n_processes: int = cpu_count(), device="cpu"):
         self.env_factory = env_factory
-        self.normalizer = ObsNormalizer(env_factory)
         self.n_processes = n_processes
         self.device = device
         self.times = times
@@ -71,9 +68,8 @@ class ParallelEnvEvaluator(Evaluator):
             done = False
             total_reward = 0
             while not done:
-                obs = self.normalizer.normalize(obs)
                 action = policy(obs)
-                obs, reward, done, _ = env.step(action)
+                obs, reward, done = env.step(action)[:3]
                 total_reward += reward
             rewards.append(total_reward)
         if self.device == "cuda":

run_cartpole.py
@@ -9,7 +9,7 @@ gym.logger.set_level(40)
 device = torch.device("cpu")
 import random
 
-def eval_model(model, env, times=1):
+def eval_model(model, env, times=1, render=False):
     #env = gym.make("CartPole-v1")
     rewards = []
     for _ in range(times):
@@ -18,6 +18,8 @@ def eval_model(model, env, times=1):
         total_reward = 0
         while not done:
             obs = torch.from_numpy(obs).float().unsqueeze(0)
+            if render:
+                env.render()
             with torch.no_grad():
                 action = int(torch.distributions.Categorical(model.forward(obs)).sample())
             obs, reward, done, _ = env.step(action)
@@ -79,6 +81,7 @@ for _ in range(200):
     elites_checked = sorted(elites_checked, key=lambda x : x[0], reverse=True)
     offspring = gen_population(elites, 999)
     offspring.append(elites_checked[0][1])
+    eval_model(elites_checked[0][1], env, render=True)
     results = [eval_model(o, env) for o in offspring]
 #print([o.state_dict()["0.bias"] for o in offspring])
 #eval_model(model)

utils.py
@@ -27,7 +27,7 @@ class ObsNormalizer:
         self.means, self.stds = self.gen_statistics(env, n_samples)
 
     def normalize(self, obs: np.ndarray):
-        return (obs - self.means) * (self.stds + ObsNormalizer.EPSILON)
+        return (obs - self.means) / (self.stds + ObsNormalizer.EPSILON)
 
     @staticmethod
     def gen_statistics(env, n):
@@ -36,7 +36,7 @@ class ObsNormalizer:
         for _ in range(n):
             action = env.action_space.sample()
             obs_dataset.append(obs)
-            obs, reward, done, _ = env.step(action)
+            obs, reward, done = env.step(action)[:3]
             if done:
                 obs = env.reset()
         obs_dataset = np.array(obs_dataset)