a1b6a9c9ad3e98f2f777363278e0e77878c6c718
Hannes Liik <hannes_liik@hotmail.com>
2019-01-12 13:09:03 +0200

    Update aggr plots

--- impl/aggregate_plots.py
+++ impl/aggregate_plots.py
@@ -14,26 +14,40 @@ parser.add_argument("--title", type=str, default="")
 parser.add_argument("-c", action="store_true")
 parser.add_argument("-s", "--save", action="store_true")
 parser.add_argument("-x", type=str, choices=["frames", "time", "generation"], default="generation")
+parser.add_argument("-l", "--labels", type=str, default=None)
+parser.add_argument("-ci", "--conf_interval", action="store_true")
 args = parser.parse_args()
 
-dirs = os.walk(args.path)
 fig = plt.figure(dpi=200)
-if not args.c:
-    for dir in list(dirs)[1:]:
-        print(dir[0])
-        df = pd.read_csv(os.path.join(dir[0], "plot.csv"))
-        with open(os.path.join(dir[0], "params.json"), "r") as fp:
-            exp_data = json.load(fp)
-        sns.lineplot(data=df, x=args.x, y="score", ci="sd", label=exp_data["exp_name"])
+paths = args.path.split(" ")
+ci = "sd" if args.conf_interval else None
+if args.labels is not None:
+    labels = iter(args.labels.split(" "))
 else:
-    df = None
-    for dir in list(dirs)[1:]:
-        print(dir[0])
-        if df is None:
+    labels = None
+for path in paths:
+    dirs = os.walk(path)
+    if not args.c:
+        for dir in list(dirs)[1:]:
+            print(dir[0])
             df = pd.read_csv(os.path.join(dir[0], "plot.csv"))
+            with open(os.path.join(dir[0], "params.json"), "r") as fp:
+                exp_data = json.load(fp)
+            sns.lineplot(data=df, x=args.x, y="score", ci=ci, label=exp_data["exp_name"])
+    else:
+        df = None
+        for dir in list(dirs)[1:]:
+            print(dir[0])
+            if df is None:
+                df = pd.read_csv(os.path.join(dir[0], "plot.csv"))
+            else:
+                df = pd.concat((df, pd.read_csv(os.path.join(dir[0], "plot.csv"))))
+
+        if labels is not None:
+            label = next(labels)
+            sns.lineplot(data=df, x=args.x, y="score", ci=ci, label=label)
         else:
-            df = pd.concat((df, pd.read_csv(os.path.join(dir[0], "plot.csv"))))
-    sns.lineplot(data=df, x=args.x, y="score", ci="sd")
+            sns.lineplot(data=df, x=args.x, y="score", ci=ci)
 if args.xlabel is not None:
     plt.xlabel(args.xlabel)
 plt.ylabel("Mean score")
